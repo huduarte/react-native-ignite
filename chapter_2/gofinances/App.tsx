09deae2e8ef488bd719df2e614b882292d2626fc
Hudson Duarte <hudsoneeto@outlook.com>
2021-06-01 08:37:28 -0300

    feat: tela inicial

---
+++ chapter_2/gofinances/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+
+import theme from './src/global/styles/theme';
+import { Dashboard } from './src/screens/Dashboard';
+import AppLoading from 'expo-app-loading';
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+} from '@expo-google-fonts/poppins';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+  });
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+  return (
+    <ThemeProvider theme={theme}>
+      <Dashboard />
+    </ThemeProvider>
+  );
+}
---
+++ chapter_2/gofinances/src/screens/Dashboard/index.tsx
@@ -0,0 +1,94 @@
+import React from 'react';
+import { HighlightCard } from '../../components/HighlightCard';
+import { TransactionCard, TransactionCardProps } from '../../components/TransactionCard';
+import { 
+  Container, 
+  Header, 
+  UserWrapper,
+  UserInfo,
+  Photo, 
+  User, 
+  UserGreeting, 
+  UserName,
+  Icon,
+  HighlightCards,
+  Transactions,
+  Title,
+  TransactionList,
+} from './styles';
+
+export interface DataListProps extends TransactionCardProps{
+  id: string;
+}
+
+export function Dashboard(){
+
+  const data: DataListProps[]  = [
+    {
+      id: '1',
+      type: 'positive',
+      title: "Desenvolvimento de sites",
+      amount: "R$ 12.000,00",
+      category: {
+        name: 'Vendas',
+        icon: 'dollar-sign'
+      },
+      date: "13/04/2020"
+    },
+    {
+      id: '2',
+      type: 'negative',
+      title: "Hamburgueria Pizzy",
+      amount: "R$ 59,00",
+      category: {
+        name: 'Alimentação',
+        icon: 'coffee'
+      },
+      date: "10/04/2020"
+    },
+    {
+      id: '3',
+      type: 'negative',
+      title: "Aluguel do apartamento",
+      amount: "R$ 1.200,00",
+      category: {
+        name: 'Casa',
+        icon: 'shopping-bag'
+      },
+      date: "10/04/2020"
+    },
+  ];
+
+  return (
+    <Container>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo source={{uri: 'https://avatars.githubusercontent.com/u/50152238?v=4' }} />
+            <User>
+              <UserGreeting>Olá, </UserGreeting>
+              <UserName>Hudson</UserName>
+            </User>
+          </UserInfo>
+          <Icon name="power" />
+        </UserWrapper>
+      </Header>
+
+      <HighlightCards >
+        <HighlightCard type="up" title="Entradas" amount="R$ 17.400,00" lastTransaction="Última entrada dia 13 de abril" />
+        <HighlightCard type="down" title="Saídas" amount="R$ 1.259,00" lastTransaction="Última saída dia 03 de abril" />
+        <HighlightCard type="total" title="Total" amount="R$ 16.141,00" lastTransaction="01 à 16 de abril" />
+      </HighlightCards>
+
+      <Transactions>
+        <Title>Listagem</Title>
+
+        <TransactionList 
+          data={data}
+          keyExtractor={item => item.id}
+          renderItem={({item}) => <TransactionCard data={item} />}
+        />
+      </Transactions>
+    </Container>
+  )
+}
