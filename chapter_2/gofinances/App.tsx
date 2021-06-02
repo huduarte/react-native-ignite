004b7dbdf25faa9f3841704700af6cc2ff89f881
Hudson Duarte <hudsoneeto@outlook.com>
2021-06-02 10:53:45 -0300

    feat: adicionando formularios com react hook forms e yup

--- chapter_2/gofinances/App.tsx
+++ chapter_2/gofinances/App.tsx
@@ -3,6 +3,8 @@ import { ThemeProvider } from 'styled-components';
 
 import theme from './src/global/styles/theme';
 import { Dashboard } from './src/screens/Dashboard';
+import { Register } from './src/screens/Register';
+import { CategorySelect } from './src/screens/CategorySelect';
 import AppLoading from 'expo-app-loading';
 import {
   useFonts,
@@ -22,7 +24,7 @@ export default function App() {
   }
   return (
     <ThemeProvider theme={theme}>
-      <Dashboard />
+      <Register />
     </ThemeProvider>
   );
 }
