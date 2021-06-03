573a074b896146b75446f618f0a1a08ad9ec958b
Hudson Duarte <hudsoneeto@outlook.com>
2021-06-03 11:49:34 -0300

    feat: listando despesas e utilizando grafico no react native

--- chapter_2/gofinances/App.tsx
+++ chapter_2/gofinances/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import 'react-native-gesture-handler';
 import 'intl';
 import 'intl/locale-data/jsonp/pt-BR';
+import {StatusBar} from 'react-native';
 import { ThemeProvider } from 'styled-components';
 
 import { NavigationContainer } from '@react-navigation/native';
@@ -31,6 +32,7 @@ export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <NavigationContainer>
+        <StatusBar barStyle="light-content" />
         <AppRoutes />
       </NavigationContainer>
     </ThemeProvider>
