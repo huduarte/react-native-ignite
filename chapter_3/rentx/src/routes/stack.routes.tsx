cafc83d6ec4b0a44ddf038d6923f8ea9f7333c2f
Hudson Duarte <hudsoneeto@outlook.com>
2021-06-20 09:04:44 -0300

    feat: criando splash screen animada

--- chapter_3/rentx/src/routes/stack.routes.tsx
+++ chapter_3/rentx/src/routes/stack.routes.tsx
@@ -8,6 +8,7 @@ import {Scheduling} from '../screens/Scheduling';
 import {SchedulingDetails} from '../screens/SchedulingDetails';
 import {SchedulingComplete} from '../screens/SchedulingComplete';
 import {MyCars} from '../screens/MyCars';
+import {Splash} from '../screens/Splash';
 
 const {Navigator, Screen} = createStackNavigator();
 
@@ -15,7 +16,12 @@ export function StackRoute(){
   return (
     <Navigator 
       headerMode="none"
+      initialRouteName="Splash"
     >
+      <Screen 
+        name="Splash"
+        component={Splash}
+      />
       <Screen 
         name="Home"
         component={Home}
